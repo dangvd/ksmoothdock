184ab2ca309adc35b738b07b7dbb2790b24c6a8d
Viet Dang <dangvd@gmail.com>
2017-04-07 22:31:57 +0100

    Initial test for config_dialog #11

---
+++ src/config_dialog_test.cc
@@ -0,0 +1,34 @@
+/*
+ * This file is part of KSmoothDock.
+ * Copyright (C) 2017 Viet Dang (dangvd@gmail.com)
+ *
+ * KSmoothDock is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * KSmoothDock is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with KSmoothDock.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "config_dialog.h"
+
+#include <QtTest>
+
+class ConfigDialogTest: public QObject {
+  Q_OBJECT
+
+ private slots:
+  void cancel();
+};
+
+void ConfigDialogTest::cancel() {
+}
+
+QTEST_MAIN(ConfigDialogTest)
+#include "config_dialog_test.moc"
